aea61a63c15a88f1ac6d7cc8510e1f50cb551fd8
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-21 13:21:55 +0200

    Initial Commit

---
+++ ToDo App/ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_AppApp.swift
+//  ToDo App
+//
+//  Created by admin on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
